77936133bd45cb02b3332f3e398b8a1dc81d40fe
Sacha schutz <sacha@labsquare.org>
2014-07-27 20:23:42 +0200

    update contrast and brightness algorythm

--- calibration.py
+++ calibration.py
@@ -1,13 +1,61 @@
-from detect_red import * 
+import bgsa 
 import os
 import PIL
 import csv 
+import time
 
 print "Debut de la calibration"
 
+
+imgb = PIL.Image.open("calibration/gland_89_9.jpg")
+imgr = PIL.Image.open("calibration/gland_89_0.jpg")
+
+
+
+gold = csv.reader(open("gold_standard.csv","r"), delimiter=";")
+
+
+
+
+goldData = dict()
+for line in gold : 
+	goldData[line[0]] = line[1]
+
+
+
+
+listDiff = []
+
 c = csv.writer(open("calibration.csv", "w"))
 
+print "============== START ============================"
 for fichier in os.listdir("calibration"):
 	image   = PIL.Image.open("calibration/"+fichier)
-	red     = get_red(image)
-	surface =	c.writerow([fichier, red.histogram()[-1]])
+	red     = bgsa.get_red(image)
+	surface = c.writerow([fichier, red.histogram()[-1]])
+	machine = float(red.histogram()[-1])
+
+	print(goldData[fichier.replace(".jpg","")])
+
+	human 	= float(goldData[fichier.replace(".jpg","")])
+
+	print fichier
+	print "machine {}".format(machine) 
+	print "human {}".format(human)
+
+	try: 
+		diff = machine / human
+	except:
+		diff = 0
+
+	if diff is not 0:
+		listDiff.append(diff)
+
+	print "diff {}".format(diff)
+
+	print "======="
+
+
+print "min: {0} - max: {1}".format(min(listDiff), max(listDiff))
+
+
--- test.py
+++ test.py
@@ -1,52 +1,9 @@
-import pygal
-import detect_red
-from progress.bar import Bar
-import numpy as np
+import bgsa
+import Image 
 
-detect_red.run("exemple2.ndpi")
+img = Image.open("calibration/gland_89_0.jpg")
 
-levelRange = range(2,7)
-splitRange = range(3,10)
+img.show()
 
-bar = Bar('Processing', max=max(splitRange))
-
-bigdata  = list()
-
-for level in levelRange:
-	data = list()
-
-	for split in splitRange:
-		results = detect_red.run("exemple2.ndpi",split, level)
-		
-		red     = results["red"]
-		total   = results["total"]
-		
-		print "red:{} total {}".format(red,total)
-
-		percent = round(red / total * 100,4) 
-		data.append(percent)
-
-	bigdata.append(data)
-		
-
-
-
-bar.finish()
-
-
-bar_chart = pygal.Bar()
-bar_chart.title = 'Differents methods using detect_red cells'
-np.array(splitRange) ** 2
-bar_chart.x_labels = map(str, np.array(splitRange) ** 2)
-
-index = 0
-
-for level in levelRange:
-	bar_chart.add("zoom {}".format(level), bigdata[index])
-	index += 1
-
-
-print bigdata
-bar_chart.render_in_browser();
-bar_chart.render_to_png("stat.png");
-bar_chart.render_to_file("stat.svg");
+# bgsa.get_red(img).show()
+bgsa.get_brown(img).show()
